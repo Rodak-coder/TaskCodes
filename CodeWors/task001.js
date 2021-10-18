ca8ccf5b1ae4baa71e8c162385395d97268d44d4
Rodak-coder <allcity8888@gmail.com>
2021-10-18 16:16:09 +0300

    gyg

--- CodeWors/task001.js
+++ CodeWors/task001.js
@@ -1,6 +1,19 @@
-class Kata {
-  static getVolumeOfCuboid(length, width, height) {
-    let volume = length * width * height;
-    return volume;
-  }
-}
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    var l = parseFloat(length);
+    var w = parseFloat(width);
+    var h = parseFloat(height);
+
+    if(isNaN(l) || isNaN(w) || isNaN(h)) return 0;
+    if(l<=0 || w<=0 ||h <= 0)  return 0;
+    
+    return l*w*h;
+  };
+
+  return Kata;
+
+})();
